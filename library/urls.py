a8b3f4c4d4481af5351734562f27fe7b15e22503
Iliazv <117463697+Iliazv@users.noreply.github.com>
2022-12-05 13:02:16 +0700

    Update urls.py

--- library/urls.py
+++ library/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.views.static import serve
-from django.conf.urls import url
+from django.conf.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('books.urls')),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    repath(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    repath(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
